a9af988f66af15128828e63d365dffa94b2f929c
Mastersigma83 <88331945+Mastersigma83@users.noreply.github.com>
2025-05-25 17:32:20 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import math
 
-st.title("🚁 Repüléstervező kalkulátor")
+st.title("AGRON Repüléstervező kalkulátor")
 
 st.markdown("""
 Ez az alkalmazás segít kiszámolni, hogy adott terület, 
